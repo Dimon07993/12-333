701320a1b90b1b321b2c1a21c755e87b4affe7b5
Dimon07993 <totti69.93@gmail.com>
2024-03-09 18:25:00 +0300

    тест 98

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,10 +4,43 @@ from utils import arrs
 
 class TestArrs(unittest.TestCase):
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+    def test_get_existing_index(self):
+        array = [1, 2, 3]
+        self.assertEqual(arrs.get(array, 1), 2)
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+    def test_get_non_existing_index(self):
+        array = [1, 2, 3]
+        self.assertEqual(arrs.get(array, 5), None)
+
+    def test_get_default_value(self):
+        array = [1, 2, 3]
+        self.assertEqual(arrs.get(array, 5, "default"), "default")
+
+    def test_get_empty_array(self):
+        array = []
+        self.assertEqual(arrs.get(array, 0), None)
+
+    def test_slice_whole_array(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array), [1, 2, 3, 4, 5])
+
+    def test_slice_start_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array, 2), [3, 4, 5])
+
+    def test_slice_end_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array, end=3), [1, 2, 3])
+
+    def test_slice_start_and_end_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array, 1, 4), [2, 3, 4])
+
+    def test_slice_negative_start_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array, -3), [3, 4, 5])
+
+    def test_slice_negative_end_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrs.my_slice(array, end=-2), [1, 2, 3])
+
